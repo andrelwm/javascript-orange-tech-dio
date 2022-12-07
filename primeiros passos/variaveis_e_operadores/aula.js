33cdcf15b068d5df68c544255a465972893b3152
André Melo <102316139+andrelwm@users.noreply.github.com>
2022-12-07 12:26:59 +0000

    Aula de variáveis, operadores e condicionais

---
+++ primeiros passos/variaveis_e_operadores/aula.js
@@ -0,0 +1,3 @@
+let numero = 10 // Variável: Após declarado um valor, pode ser alterado se desejado
+var numero2 = 5 // Variável
+const pi = 3.14 // Constante: Após declarado um valor, não pode ser alterado
