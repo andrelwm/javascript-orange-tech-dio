9f9b4423bcab21eaed9da9ef216aba7a685934f3
André Melo <102316139+andrelwm@users.noreply.github.com>
2022-12-10 18:31:13 +0000

    Aula sobre Objetos e Classes

--- primeiros passos/Objetos_e_Classes/aula.js
+++ primeiros passos/Objetos_e_Classes/aula.js
@@ -1,19 +1,30 @@
 class Pessoa {
     nome;
     idade;
+    anoDeNascimento;
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2022 - idade;
+    }
 
     descrever(){
-        console.log(`Meu nome é ${this.nome} e eu tenho ${this.idade} anos.`);
+        console.log(`Meu nome é ${this.nome} e eu tenho ${this.idade} anos.\nMeu anode nascimento é ${this.anoDeNascimento}.`);
     }
 }
 
-const erica = new Pessoa();
-erica.nome = 'Erica de Oliveira Xavier';
-erica.idade = 23;
+function compararPessoas(p1, p2){
+    if (p1.idade > p2.idade) {
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}!`);
+    } else if (p1.idade < p2.idade) {
+        console.log(`${p2.nome} é mais velho(a) que ${p1.nome}!`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade!`)
+    }
+}
 
-const andre = new Pessoa();
-andre.nome = 'André Luiz Wanderley de Melo';
-andre.idade = 29;
+const erica = new Pessoa('Erica de Oliveira Xavier', 23);
+const andre = new Pessoa('André Luiz Wanderley de Melo', 29);
 
-erica.descrever();
-andre.descrever();
+compararPessoas(erica, andre);
