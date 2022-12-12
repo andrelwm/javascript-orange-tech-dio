21887592eaa51c80824171d1b840e46c90fce617
André Melo <102316139+andrelwm@users.noreply.github.com>
2022-12-12 17:45:18 +0000

    Atividade sobre Objetos e Classes

---
+++ primeiros passos/Praticando/Objetos_e_Classes_2.js
@@ -0,0 +1,29 @@
+/*
+    2 - Crie uma classe para representar pessoas.
+    Para cada pessoa teremos os atributos nome, peso e altura.
+    As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura)).
+    Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça a José para dizer o valor do seu IMC.
+
+*/
+
+class Pessoa {
+    nome;
+    peso;
+    altura;
+
+    constructor(nome, peso, altura){
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    imc() {
+        return this.peso / (this.altura * this.altura)
+    }
+}
+
+const jose = new Pessoa('José', 70, 1.75);
+
+imcJose = jose.imc();
+
+console.log(imcJose);
